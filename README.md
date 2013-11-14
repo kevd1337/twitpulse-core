8f47869ac3cb33ec01b72d116388f7aba5aef619
kevd1337 <kevd1337@gmail.com>
2013-11-14 13:31:16 -0600

    Setting up initial project structure, a maven/pom dependencies, adding local jar dependencies, and a dummy home page for future use

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 twitpulse-core
 ==============
 
-TwitPulse's twitter sentiment analysis core library code
+TwitPulse's twitter sentiment analysis core library code. Also includes a webapplication wrapper around the library.
+
+## Running the web application locally
+
+First build with:
+
+    $mvn clean install
+    
+Or alternatively import project into eclipse, make sure it's a maven java project.
+
+
+To run it on command line:
+
+    $ java -cp target/classes:target/dependency/* org.twitpulse.webapp.Main
+
+Or alternatively using foreman:
+
+   $ foreman start
+   
+Or alternatively you can setup an eclipse run configuration to make use of org.twitpulse.webapp.Main
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Twitter Sentiment Training Data
+
+Some training data is omitted from repo to save space (and probably due to licensing as well). 
+
+For the tweet sentiment classifier, see this training data:
+http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip
---
+++ src/org/twitpulse/webapp/Main.java
@@ -0,0 +1,41 @@
+package org.twitpulse.webapp;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * 
+ * This class launches the web application in an embedded Jetty container. This is the entry point to your application.
+ * The Java command that is used for launching should fire this main method.
+ * 
+ * @author kevd1337
+ */
+public class Main {
+
+    /**
+     * Main entry point of web application
+     * 
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "webapp/";
+
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "5000";
+        }
+
+        Server server = new Server(Integer.valueOf(webPort));
+        WebAppContext root = new WebAppContext();
+
+        root.setContextPath("/");
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+        root.setParentLoaderPriority(true);
+        server.setHandler(root);
+
+        server.start();
+        server.join();
+    }
+}
